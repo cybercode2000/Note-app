c1433583b7ab64131d5a3bdb60433f3091cfdc99
cybercode2000 <damicodd123@gmail.com>
2020-08-02 11:09:28 -0400

    solve the problem

---
+++ app.js
@@ -0,0 +1,116 @@
+var url = require("url");
+var fs = require("fs");
+const { parse } = require("querystring");
+
+var body = "";
+const date = new Date();
+
+const allFiles = fs.readFileSync("./note.json", "utf8");
+
+module.exports = {
+  handleRequest: function(request, response) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    var path = url.parse(request.url).pathname;
+    const Data = JSON.parse(allFiles);
+
+    switch (path) {
+      case "/":
+        if (request.method == "GET") {
+          response.end(allFiles);
+          break;
+        } else if (request.method == "POST") {
+          var body = "";
+          request.on("data", function(chunk) {
+            body += chunk;
+          });
+
+          request.on("end", function() {
+            response.writeHead(200);
+            const newBody = JSON.parse(body);
+
+            if (
+              typeof newBody.category == "undefined" &&
+              typeof newBody.note == "undefined"
+            ) {
+              response.end("Category Path and Note Path is not define");
+            } else if (typeof newBody.category == "undefined") {
+              response.end("Category Path is not define");
+            } else if (typeof newBody.note == "undefined") {
+              response.end("Note Path is not define");
+            } else {
+              if (
+                newBody.category == "work" ||
+                newBody.category == "social" ||
+                newBody.category == "personal"
+              ) {
+                const newNote = {
+                  id: date,
+                  category: newBody.category,
+                  note: newBody.note
+                };
+
+                Data.push(newNote);
+                fs.writeFile("./note.json", JSON.stringify(Data), err => {
+                  if (err) throw err;
+                  response.end("Saved Successfully");
+                });
+              } else {
+                response.end(
+                  "Please Kindly Select a Valid Category. Example - social, personal, work"
+                );
+              }
+            }
+          });
+
+          break;
+        } else {
+          response.end("not found");
+        }
+      case "/personal":
+        const personal = Data.filter(data => {
+          return data.category === "personal";
+        });
+
+        if (personal.length >= 1) {
+          response.end(JSON.stringify(personal));
+        } else {
+          response.writeHead(200);
+          response.end("No Data in the Directory!!!!!");
+        }
+
+        break;
+      case "/work":
+        const work = Data.filter(data => {
+          return data.category === "work";
+        });
+
+        if (work.length >= 1) {
+          response.end(JSON.stringify(work));
+        } else {
+          response.writeHead(200);
+          response.end("No Data in the Directory!!!!!");
+        }
+
+        break;
+
+      case "/social":
+        const social = Data.filter(data => {
+          return data.category === "social";
+        });
+
+        if (social.length >= 1) {
+          response.end(JSON.stringify(social));
+        } else {
+          response.writeHead(200);
+          response.end("No Data in the Directory!!!!!");
+        }
+
+        break;
+
+      default:
+        response.writeHead(404);
+        response.write("Route not defined");
+        response.end();
+    }
+  }
+};
--- server.js
+++ server.js
@@ -1,27 +1,5 @@
-const http = require("http");
-const fs = require("fs");
+var http = require("http");
+var app = require("./app");
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/" && req.method == "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        fs.readFile('./views/index.html', null, function (error, data) {
-            if (error) {
-                res.writeHead(404);
-                res.write('Whoops! File not found!');
-            } else {
-                res.write(data);
-            }
-            res.end();
-        })
-    }
-    else {
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        res.end("PAGE NOT FOUND")
-    }
-}) 
 
-PORT = process.env.PORT || 3000
-
-server.listen(PORT, () => {
-    console.log(`Note App is Running @ PORT ${PORT}`);
-})
+http.createServer(app.handleRequest).listen(8000);
